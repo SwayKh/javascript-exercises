383ffc6589677d7c7666276e1c220fcf4c4b2234
SwayKh <khatrisumit712@gmail.com>
2023-10-06 21:49:26 +0530

    Complete Exercise 1 and 2 of javascript Exercise
    Completed the repeatString exercise of chapter 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (input, noOfTimes) {
 
+    finalValue = ''
+    if (noOfTimes < 0) {
+        return 'ERROR'
+    }
+    for (let i = 1; i <= noOfTimes; i++) {
+        finalValue += input
+    }
+    return finalValue;
 };
+
+repeatString('hey', 5);
 
 // Do not edit below this line
 module.exports = repeatString;
