aa61eaa8655111576b258807297bfd5bed25eb3c
SwayKh <khatrisumit712@gmail.com>
2023-10-06 22:07:14 +0530

    Complete exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    let inputArray = String(word).split('');
+    let reverseArray = inputArray.reverse();
+    return reverseArray.join('')
+    // return inputArray.reverse().join()
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
