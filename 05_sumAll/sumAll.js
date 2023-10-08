17e3e9037f18d459be919192ea7b0829232a599a
SwayKh <khatrisumit712@gmail.com>
2023-10-08 21:55:58 +0530

    Complete exercise 05
    This one was easy, And the first time
    I wrote psuedo code on a papers and went
    through the steps to solve before writting code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function () {
+    let twoArgs = Array.from(arguments);
+    let totalNumbers = (twoArgs[0] + twoArgs[1]);
+    let sum = 0;
+    console.log(typeof twoArgs[0] + " and " + typeof twoArgs[1])
+
+    if (twoArgs[0] < 0 || twoArgs[1] < 0) {
+        return 'ERROR'
+    }
+    else if (typeof twoArgs[0] == 'number' && typeof twoArgs[1] == 'number') {
+        for (let i = 0; i < totalNumbers; i++) {
+            sum += i
+        }
+        return sum
+    }
+    else {
+        return 'ERROR'
+    }
+
 
 };
+
+console.log(sumAll(123, "1"));
 
 // Do not edit below this line
 module.exports = sumAll;
