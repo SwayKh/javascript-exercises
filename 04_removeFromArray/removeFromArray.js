28a1ef8324251207bce56a3d724029bf66a7fb57
SwayKh <khatrisumit712@gmail.com>
2023-10-08 21:28:47 +0530

    Complete exercise 4, Had to use ChatGPT cause I couldn't solve it, Sorry

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,36 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, toRemove) {
+    let newArray = []
+    removeArgs = Array.from(arguments).sort((a, b) => a - b).splice(1)
+    // removeArgs = [].slice.call(arguments)
+    // console.log("To Remove: " + toRemove)
+    console.log("To Remove Argument: " + removeArgs + " of type: " + typeof removeArgs)
+    console.log("Argument Length: " + removeArgs.length)
+    console.log(removeArgs[1])
 
+    // arrayCounter = 0
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     for (let j = 0; j < removeArgs.length; j++) {
+    //         if (arr[i] === removeArgs[j]) {
+    //             newArray = arr.filter(function (newArray) {
+    //                 return newArray !== arr[i];
+    //             })
+    //         }
+
+    //     }
+
+    // }
+    for (let i = 0; i < arr.length; i++) {
+        if (removeArgs.indexOf(arr[i]) === -1) {
+            newArray.push(arr[i]);
+        }
+
+    }
+    return newArray;
 };
+
+
+// console.log(removeFromArray([1, 2, 3, 4], 3, 4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
