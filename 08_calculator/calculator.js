0a207c215e9a584d07186510dabb5acc3874b77a
SwayKh <khatrisumit712@gmail.com>
2023-10-15 22:28:22 +0530

    Complete exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+  let total = 1;
+  for (let i = 0; i < args.length; i++) {
+    total *= args[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let total = 1
+  if (n === 0) {
+    return 1;
+  }
+  for (let i = 1; i <= n; i++) {
+    total = total * i;
+  }
+
+  return total
 };
 
 // Do not edit below this line
