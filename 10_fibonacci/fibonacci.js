fdc22b7cf7f738078e1f9fa3dcd2de78f9a19e58
SwayKh <khatrisumit712@gmail.com>
2023-10-16 19:28:43 +0530

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) {
+        return "OOPS"
+    }
+    const value = createFibonacci(index - 1)
 
+    return value[value.length - 1]
 };
+
+function createFibonacci(last) {
+    let finalArray = [0, 1]
+    for (let i = 0; i < last; i++) {
+        nextIndex = i + 1
+        finalArray.push(finalArray[i] + finalArray[nextIndex])
+    }
+    return finalArray;
+}
+
+console.log(createFibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
