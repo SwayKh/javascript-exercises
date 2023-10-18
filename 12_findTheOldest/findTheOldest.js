3dcb8936f060652102fa909ada9ec4f36d2bce26
SwayKh <khatrisumit712@gmail.com>
2023-10-18 14:17:18 +0530

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+    const age = peopleArray.map(people => {
+        if (!people.yearOfDeath) {
+            const d = new Date()
+            people.yearOfDeath = d.getFullYear();
+        }
+        people["age"] = people.yearOfDeath - people.yearOfBirth
+        return people
+    })
 
+    const oldestAge = age.sort((a, b) => {
+        if (a.age > b.age) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+
+    return oldestAge[oldestAge.length - 1]
+
+    // return age.sort()[age.length - 1]
 };
 
 // Do not edit below this line
